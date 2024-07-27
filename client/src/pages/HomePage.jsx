ded7d151c2cbdaeebdd0f04febc15f00d6e9e441
shagnikpaul <shagnikpaul.772@gmail.com>
2024-07-27 18:34:39 +0530

    ⚙️ small tweaks

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Heading from '../components/homepage/Heading'
 import NavBarTop from '../components/NavBarTop'
 import data from '../utils/sample_data';
 
-import Body from '../components/homepage/draggable-list-test/Body';
+
 // import {Chip} from '../components/homepage/Chip'
 
 
