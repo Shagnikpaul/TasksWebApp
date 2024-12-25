290573ba0fa88334bcef513adc72d34df2784ab9
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-12-25 11:02:55 +0530

    fixed res after successful connection

--- BackEnd/backend/connection/connection.js
+++ BackEnd/backend/connection/connection.js
@@ -5,7 +5,8 @@ const conn = async (req, res) => {
         dotenv.config()
         console.log("mongo connection string : ", process.env.mongo);
         await mongoose.connect(process.env.mongo).then(() => {
-            console.log("Connected")
+            // console.log("Connected")
+            res.status(200).json({"message":"Connected"});
         })
     } catch (error) {
         res.status(400).json({
